6985343c634e4246ab8d614d50cb86295bdba925
Leonardo Bispo <leonardobispo1000@gmail.com>
2020-11-30 10:52:41 -0300

    Commi Project all ready

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
+
 import {Router} from 'react-router-dom';
+
 import {ToastContainer} from 'react-toastify';
+
 import {Provider} from 'react-redux';
+
 import {PersistGate} from 'redux-persist/integration/react';
 
 import GlobalStyles from './styles/globalStyles';
+
 import Header from './components/header';
+
 import Routes from './routes';
+
 import history from './services/history';
+
 import store, { persistor } from './store'
+
 function App() {
   return (
     <Provider store={store}>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+//Colocando oque vai renderizar na aplicação
 ReactDOM.render(
   <React.StrictMode>
     <App />
